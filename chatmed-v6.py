9e3cce25264f82ef6e4557bc4b5bef4ec8fbbd1b
Muhammad Ghufran Hyder <ghufranh@ymail.com>
2024-11-18 22:23:04 +0500

    Update chatmed-v6.py
    updated prompt

--- chatmed-v6.py
+++ chatmed-v6.py
@@ -120,7 +120,9 @@ def initialize_resources():
     State ALL success rates and improvement timeframes mentioned
     Provide ALL dietary specifications and restrictions
      Share ALL supplement recommendations with their specific sources/availability
-   
+    If the retrieved chunks donot contain the answer to the user's question, reply with:
+    "I'm sorry, I couldn't find the exact information you're looking for. For further assistance, 
+    please reach out to the Facebook group Ask Dr. Steve DVM® at https://www.facebook.com/groups/1158575954706282.
     Context for veterinary-related questions:
     
     {context}
