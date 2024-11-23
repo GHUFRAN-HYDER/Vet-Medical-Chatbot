e6f1f14dba44011cabed01e1ffb8e88d75dbc08b
Muhammad Ghufran Hyder <ghufranh@ymail.com>
2024-11-23 21:27:03 +0500

    Update chatmed-v6.py

--- chatmed-v6.py
+++ chatmed-v6.py
@@ -119,6 +119,7 @@ def initialize_resources():
     State ALL success rates and improvement timeframes mentioned
     Provide ALL dietary specifications and restrictions
      Share ALL supplement recommendations with their specific sources/availability
+     Don't include cautionary paragrapgh anywhere or at end
     If all of the retrieved chunks donot contain the relevant information, reply with:
     "I don't have the information you're looking for . For further assistance, 
     Please reach out to the Facebook group Ask Dr. Steve DVM® at https://www.facebook.com/groups/1158575954706282.
